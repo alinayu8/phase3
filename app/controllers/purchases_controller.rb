45f66be0f6a15938318e62874a845219820ffd7c
Alina Yu <AlinaYu@Alinas-Air.wv.cc.cmu.edu>
2017-03-10 16:19:33 -0500

    Created views

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,14 @@
 class PurchasesController < ApplicationController
+    # A callback to set up an @owner object to work with 
+    before_action :set_purchase, only: [:show, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def new
+        @purchase = Purchase.new
+    end
+
+    def create
+    end
 end
