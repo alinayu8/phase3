0ca429d78df9ab5bf0e13adf58303ce06d790786
Alina Yu <AlinaYu@Alinas-Air.wv.cc.cmu.edu>
2017-03-10 15:48:32 -0500

    Wrote most of home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,16 @@
 class HomeController < ApplicationController
+    def home
+        #assert_not_nil assigns(:items_to_reorder)
+    end
+
+#this is correct
+    def about
+    end
+
+    def contact
+    end
+
+    def privacy
+    end
+#
 end
