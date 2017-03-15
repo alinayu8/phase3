b335c6f564eaf17465d8293b882a72b6683d7a98
Alina Yu <AlinaYu@Alinas-Air.wv.cc.cmu.edu>
2017-03-14 22:06:51 -0400

    Completed level 2 tests

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -6,14 +6,16 @@ class ItemPricesController < ApplicationController
     end
 
     def new
+        @item_price = ItemPrice.new
     end
 
     def create
         @item_price = ItemPrice.new(item_price_params)
+        @item_price.start_date = Date.current
         if @item_price.save
             #if saved to database
             @item = Item.find(@item_price.item_id)
-            flash[:notice] = "Changed the price of #{Item.find(@item_price.item_id).name}."
+            flash[:notice] = "Changed the price of #{@item.name}."
             redirect_to item_path(@item)
         else
             #return to the 'new' form
