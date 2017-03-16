64e92142e79cf0d0a96ded90cfa45ef89cbff41d
Alina Yu <AlinaYu@Alinas-Air.wv.cc.cmu.edu>
2017-03-16 01:03:58 -0400

    Implemented navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,16 @@ $(function() {
 });
 
 $(function(){ $(document).foundation(); });
+
+$('#primary-menu').on(
+    'show.zf.dropdownmenu', function() {
+      var dropdown = $(this).find('.is-dropdown-submenu');
+      dropdown.css('display', 'none');
+      dropdown.fadeIn('slow');
+  });
+  $('#primary-menu').on(
+    'hide.zf.dropdownmenu', function() {
+      var dropdown = $(this).find('.is-dropdown-submenu');
+      dropdown.css('display', 'inherit');
+      dropdown.fadeOut('slow');
+  });
